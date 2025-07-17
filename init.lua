8228bd9cda6f64634239cdff4bf3cb721af9de3e
Brent Pappas <pappasbrent@gmail.com>
2025-07-17 14:57:59 -0400

    Add local line number variable

--- init.lua
+++ init.lua
@@ -173,7 +173,8 @@ vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
+    local line = mark[1]
+    if line > 0 and line <= lcount then
       pcall(vim.api.nvim_win_set_cursor, 0, mark)
     end
   end,
