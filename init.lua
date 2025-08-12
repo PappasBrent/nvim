5b2d257b9f02dd0b03c62ea1bdc50e1ca06bdbc8
Brent Pappas <pappasbrent@gmail.com>
2025-08-12 10:55:18 -0400

    Set colorscheme

--- init.lua
+++ init.lua
@@ -3,8 +3,7 @@
 -- author: Radley E. Sidwell-lewis
 -- ================================================================================================
 
--- Theme & transparency
--- vim.cmd.colorscheme("unokai")
+-- Transparency
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
@@ -577,3 +576,4 @@ vim.pack.add({
     "https://github.com/lewis6991/gitsigns.nvim",   -- Git signs
 })
 
+vim.cmd.colorscheme("catppuccin-frappe")
