58c1791705cc8011093ce7a7dfaaa8e6ed5a5114
Brent Pappas <pappasbrent@gmail.com>
2025-08-23 15:59:39 -0400

    Add mapping to close completion window

--- plugin/nvim-cmp.lua
+++ plugin/nvim-cmp.lua
@@ -26,7 +26,13 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-Space>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.close()
+      else
+        cmp.open()
+      end
+    end, { "i", "s" }),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ["<C-j>"] = cmp.mapping(function(fallback)
