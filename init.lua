4f3e73a60c44f21fc65b60c9eb93c50dab47cbbc
Brent Pappas <pappasbrent@gmail.com>
2025-11-21 19:20:33 -0500

    Avoid restoring position for git commits, etc.
    Avoid restoring position for git commits, rebases, binary files, and
    diffs. See https://neovim.io/doc/user/usr_05.html#restore-cursor.

--- init.lua
+++ init.lua
@@ -196,7 +196,10 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lcount = vim.api.nvim_buf_line_count(0)
     local line = mark[1]
-    if line > 0 and line <= lcount then
+    local ft = vim.bo.filetype
+    if line > 0 and line <= lcount
+      and vim.fn.index({ "commit", "gitrebase", "xxd" }, ft) == -1
+      and not vim.o.diff then
       pcall(vim.api.nvim_win_set_cursor, 0, mark)
     end
   end,
